3ee066df833acf21dfd5fa9411586f088a1042fe
Tharindu Eranga <tharindu.e@bsfdv.com>
2023-02-28 15:33:00 +0400

    feat: add new sout

--- src/main/java/com/bsf/commitizen/maven/SpringbootApplication.java
+++ src/main/java/com/bsf/commitizen/maven/SpringbootApplication.java
@@ -3,5 +3,6 @@ package com.bsf.commitizen.maven;
 public class SpringbootApplication {
     public static void main(String[] args) {
         System.out.println("abc");
+        System.out.println("123");
     }
 }
