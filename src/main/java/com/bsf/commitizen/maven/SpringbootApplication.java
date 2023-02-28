e4fe83c1589b90767fe4012fce0125a76dcd5c95
Tharindu Eranga <tharindu.e@bsfdv.com>
2023-02-28 17:01:09 +0400

    feat: add one commit

--- src/main/java/com/bsf/commitizen/maven/SpringbootApplication.java
+++ src/main/java/com/bsf/commitizen/maven/SpringbootApplication.java
@@ -4,5 +4,6 @@ public class SpringbootApplication {
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println("123");
+        System.out.println("abc2");
     }
 }
